0cca419b544fcfd1d86680696fde9ad10d64886d
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-07-08 22:31:43 +0530

    project initialization

---
+++ src/main/java/com/behl/strongbox/CloudAgnosticStorageGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.behl.strongbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAgnosticStorageGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAgnosticStorageGatewayApplication.class, args);
+	}
+
+}
