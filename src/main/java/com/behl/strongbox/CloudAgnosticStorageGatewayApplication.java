b8687d6c289784e4483a1badb2d8d129b3e46155
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-07-09 19:58:07 +0530

    updating AWS properties validation

--- src/main/java/com/behl/strongbox/CloudAgnosticStorageGatewayApplication.java
+++ src/main/java/com/behl/strongbox/CloudAgnosticStorageGatewayApplication.java
@@ -6,7 +6,9 @@ import org.apache.commons.lang3.BooleanUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.amazonaws.util.StringUtils;
 import com.behl.strongbox.configuration.properties.AwsConfigurationProperties;
+import com.behl.strongbox.configuration.properties.AzureConfigurationProperties;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,12 +20,31 @@ public class CloudAgnosticStorageGatewayApplication {
 		var applicationContext = SpringApplication.run(CloudAgnosticStorageGatewayApplication.class, args);
 
 		var awsConfigurationProperties = applicationContext.getBean(AwsConfigurationProperties.class);
+		var azureConfigurationProperties = applicationContext.getBean(AzureConfigurationProperties.class);
 
 		if (allDisabled(awsConfigurationProperties)) {
 			log.error(
 					"Atleast 1 Storage-service provider must be enabled : {} : Go to 'application.properties' file to update configuration",
 					LocalDateTime.now());
 			exit();
+		}
+
+		validateConfiguration(awsConfigurationProperties);
+	}
+
+	private static void validateConfiguration(final AwsConfigurationProperties awsConfigurationProperties) {
+		if (BooleanUtils.isTrue(awsConfigurationProperties.getEnabled())) {
+			if (StringUtils.isNullOrEmpty(awsConfigurationProperties.getS3().getBucketName())) {
+				log.error("Bucket-name cannot be left empty under property 'com.behl.strongbox.aws.s3.bucket-name'");
+				exit();
+			}
+			if (!StringUtils.isNullOrEmpty(awsConfigurationProperties.getAccessKey())
+					&& !StringUtils.isNullOrEmpty(awsConfigurationProperties.getSecretAccessKey())) {
+				if (StringUtils.isNullOrEmpty(awsConfigurationProperties.getS3().getRegion())) {
+					log.error("A valid region must be provided corresponding to configured S3 Bucket");
+					exit();
+				}
+			}
 		}
 	}
 
--- src/main/java/com/behl/strongbox/configuration/AwsS3Configuration.java
+++ src/main/java/com/behl/strongbox/configuration/AwsS3Configuration.java
@@ -15,7 +15,9 @@ import com.amazonaws.util.StringUtils;
 import com.behl.strongbox.configuration.properties.AwsConfigurationProperties;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 @AllArgsConstructor
 @EnableConfigurationProperties(value = AwsConfigurationProperties.class)
@@ -29,6 +31,7 @@ public class AwsS3Configuration {
 			if (!StringUtils.isNullOrEmpty(awsConfigurationProperties.getAccessKey())
 					&& !StringUtils.isNullOrEmpty(awsConfigurationProperties.getSecretAccessKey())
 					&& !StringUtils.isNullOrEmpty(awsConfigurationProperties.getS3().getRegion())) {
+				validateRegion(awsConfigurationProperties.getS3().getRegion());
 				AWSCredentials awsCredentials = new BasicAWSCredentials(awsConfigurationProperties.getAccessKey(),
 						awsConfigurationProperties.getSecretAccessKey());
 				return AmazonS3ClientBuilder.standard()
@@ -40,6 +43,15 @@ public class AwsS3Configuration {
 			}
 		}
 		return null;
+	}
+
+	private void validateRegion(final String region) {
+		try {
+			Regions.fromName(region);
+		} catch (final IllegalArgumentException exception) {
+			log.error("A valid region must be provided corresponding to configured S3 Bucket");
+			System.exit(0);
+		}
 	}
 
 }
