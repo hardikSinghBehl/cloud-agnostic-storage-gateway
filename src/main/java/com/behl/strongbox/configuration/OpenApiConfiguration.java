7d0c4b9a3fba6c41cdd51b130060fd1e059a80dc
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-07-09 18:51:46 +0530

    configuring security to OpenAPI bean

--- src/main/java/com/behl/strongbox/configuration/OpenApiConfiguration.java
+++ src/main/java/com/behl/strongbox/configuration/OpenApiConfiguration.java
@@ -6,9 +6,12 @@ import org.springframework.context.annotation.Configuration;
 
 import com.behl.strongbox.configuration.properties.OpenApiConfigurationProperties;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import lombok.AllArgsConstructor;
 
 @Configuration
@@ -21,11 +24,15 @@ public class OpenApiConfiguration {
 	@Bean
 	public OpenAPI customOpenAPI() {
 		final var openApiProperties = openApiConfigurationProperties.getOpenApi();
+		final var security = openApiProperties.getSecurity();
 		final var contact = openApiProperties.getContact();
 		final var info = new Info().title(openApiProperties.getTitle()).version(openApiProperties.getApiVersion())
 				.description(openApiProperties.getDescription())
 				.contact(new Contact().email(contact.getEmail()).name(contact.getName()).url(contact.getUrl()));
 
-		return new OpenAPI().info(info);
+		return new OpenAPI().info(info).addSecurityItem(new SecurityRequirement().addList(security.getName()))
+				.components(new Components().addSecuritySchemes(security.getName(),
+						new SecurityScheme().name(security.getName()).type(SecurityScheme.Type.HTTP)
+								.scheme(security.getScheme()).bearerFormat(security.getBearerFormat())));
 	}
 }
