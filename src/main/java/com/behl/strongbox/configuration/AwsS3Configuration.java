c0af1e9d575220754078132d537ac85c328f2745
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-07-16 13:38:02 +0530

    refactor: externalising s3 non primary beans

--- src/main/java/com/behl/strongbox/configuration/AwsS3Configuration.java
+++ src/main/java/com/behl/strongbox/configuration/AwsS3Configuration.java
@@ -9,14 +9,11 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.util.StringUtils;
 import com.behl.strongbox.configuration.properties.AwsConfigurationProperties;
-import com.behl.strongbox.configuration.properties.DigitalOceanSpacesConfigurationProperties;
-import com.behl.strongbox.configuration.properties.S3NinjaConfigurationProperties;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -24,16 +21,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Configuration
 @AllArgsConstructor
-@EnableConfigurationProperties(value = { AwsConfigurationProperties.class, S3NinjaConfigurationProperties.class,
-		DigitalOceanSpacesConfigurationProperties.class })
+@EnableConfigurationProperties(value = { AwsConfigurationProperties.class })
 public class AwsS3Configuration {
 
 	private final AwsConfigurationProperties awsConfigurationProperties;
-	private final S3NinjaConfigurationProperties s3NinjaConfigurationProperties;
-	private final DigitalOceanSpacesConfigurationProperties digitalOceanSpacesConfigurationProperties;
 
-	@Bean("amazonS3")
 	@Primary
+	@Bean("amazonS3")
 	public AmazonS3 amazonS3() {
 		if (Boolean.TRUE.equals(awsConfigurationProperties.getEnabled())) {
 			if (!StringUtils.isNullOrEmpty(awsConfigurationProperties.getAccessKey())
@@ -51,35 +45,6 @@ public class AwsS3Configuration {
 			}
 		}
 		return null;
-	}
-
-	@Bean("emulatedAmazonS3")
-	public AmazonS3 emulatedAmazonS3() {
-		if (Boolean.TRUE.equals(s3NinjaConfigurationProperties.getEnabled())) {
-			var endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
-					s3NinjaConfigurationProperties.getS3().getEndpoint(), Regions.AP_SOUTH_1.getName());
-			var awsCredentials = new BasicAWSCredentials(s3NinjaConfigurationProperties.getAccessKey(),
-					s3NinjaConfigurationProperties.getSecretAccessKey());
-			return AmazonS3ClientBuilder.standard().withEndpointConfiguration(endpointConfiguration)
-					.withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).withPathStyleAccessEnabled(true)
-					.build();
-		}
-		return null;
-
-	}
-
-	@Bean("digitalOceanSpace")
-	public AmazonS3 digitalOceanSpace() {
-		if (Boolean.TRUE.equals(digitalOceanSpacesConfigurationProperties.getEnabled())) {
-			var endpointConfiguration = new AwsClientBuilder.EndpointConfiguration(
-					digitalOceanSpacesConfigurationProperties.getEndpoint(), Regions.AP_SOUTH_1.getName());
-			var awsCredentials = new BasicAWSCredentials(digitalOceanSpacesConfigurationProperties.getAccessKey(),
-					digitalOceanSpacesConfigurationProperties.getSecretKey());
-			return AmazonS3ClientBuilder.standard().withEndpointConfiguration(endpointConfiguration)
-					.withCredentials(new AWSStaticCredentialsProvider(awsCredentials)).build();
-		}
-		return null;
-
 	}
 
 	private void validateRegion(final String region) {
