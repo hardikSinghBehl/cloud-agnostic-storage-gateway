42d05ce1afe30823c9c1db551684555a44d96ef0
Hardik Singh Behl <hardikbehl@Hardiks-MacBook-Air.local>
2022-07-16 14:33:46 +0530

    fix: nullable custom metadata

--- src/main/java/com/behl/strongbox/utility/JsonUtil.java
+++ src/main/java/com/behl/strongbox/utility/JsonUtil.java
@@ -7,10 +7,10 @@ import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
 
+import com.amazonaws.util.StringUtils;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import lombok.NonNull;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -19,7 +19,9 @@ public class JsonUtil {
 	private JsonUtil() {
 	}
 
-	public static Map<String, Object> toMap(@NonNull final String jsonData) {
+	public static Map<String, Object> toMap(final String jsonData) {
+		if (StringUtils.isNullOrEmpty(jsonData))
+			return null;
 		final var objectMapper = new ObjectMapper();
 		Map<String, Object> map = new HashMap<String, Object>();
 		try {
